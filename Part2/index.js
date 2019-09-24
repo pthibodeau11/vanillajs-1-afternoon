de58e088bc071ca3f9e26c224cf384abe21add5c
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-09-23 21:03:10 -0500

    completed vanilla JS afternoon proj

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,13 @@
-console.log("LIVE")
+console.log("The house always wins!")
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+
+// console.log(idInput)
+// console.log(colorInput)
+
+function setCard() {
+    const card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value;
+}
